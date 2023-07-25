b4c3cb132b5fe6fb4eb9fab0e4ca9c825ed9d322
sachinhake009 <sachinhake009@gmail.com>
2023-07-25 20:54:57 +0530

    opreters js
    opreters js

---
+++ 05_operatorA.js
@@ -0,0 +1,54 @@
+
+console.log(`====================================================Assignment A======================================================`);
+
+String = "JavaScript"
+var squareOfWordLenght = function(){
+console.log(`"JavaScript" Length = ${String.length}`);
+var square= String.length*String.length;
+console.log(`"JavaScript" Squre= ${square}`);
+
+console.log(`-----------------------------------------------------------------------------------------------------------------------`);
+}
+squareOfWordLenght();
+
+String ="Google Chrome"
+var squareOfWordLenght= function(){
+console.log(`"Google Chrome" Length = ${String.length}`);
+var square=String.length*String.length;
+console.log(`"Google Chrome" Squre =${square}`);
+
+console.log(`----------------------------------------------------------------------------------------------------------------------- `);
+}
+squareOfWordLenght();
+
+
+String="Developer Smart"
+var squareOfWordLenght = function(){
+console.log(`"Developer Smart" Length = ${String.length} `);
+var square = String.length*String.length;
+console.log(`"Developer Smart" Squre = ${square}`);
+}
+squareOfWordLenght();
+
+console.log(`============================================================2===========================================================`);
+
+String = "I am Angular Developer"
+var sachin=  function(){
+console.log(`"I am Angular Developer" Length= ${String.length}`);
+var totalChar=String.split(" ");
+totalWordCount=totalChar.length;
+console.log(`"I am Angular Developer" Word = ${totalWordCount} `);
+
+console.log(`-----------------------------------------------------------2.1-----------------------------------------------------------`);
+
+var Division=String.length/totalChar.length;
+console.log(`"I am Angular Developer" Length Divide by Words = ${Division}`);
+
+console.log(`-----------------------------------------------------------2.2-----------------------------------------------------------`);
+
+var Multiplication=String.length*totalChar.length;
+console.log(`"I am Angular Developer" Length Multiplacation by Words = ${Multiplication}`);
+}
+sachin();
+
+console.log(`=========================================================================================================================`);
