3d6ed81d008f22c9ab8ea5c6672f2144a2757da5
sachinhake009 <sachinhake009@gmail.com>
2023-07-27 18:40:39 +0530

    console js
    console js

--- 05_console.js
+++ 05_console.js
@@ -1,3 +1,44 @@
 
 
-0==``
+console.log(`------------------------------------Assignmet 2 ----------------------------------`);
+
+result = 0==``;
+console.log(result);
+console.log(`result is true space is get converted to number implicitly`);
+
+console.log(`-----------------------------------------------------------------------------`);
+
+result = 0==`0`;
+console.log(result);
+console.log(`result is true 0 is get converted to string implicitly`);
+
+console.log(`-----------------------------------------------------------------------------`);
+
+result = 0==false;
+console.log(result);
+console.log(`result is true , false is get converted to number`);
+
+console.log(`-----------------------------------------------------------------------------`);
+
+result = null==undefined;
+console.log(result);
+console.log(`result get true beacuse loosely equal as both having different type`);
+
+console.log(`-----------------------------------------------------------------------------`);
+
+result = 1==[1];
+console.log(result);
+console.log(`resulat is true both are df same type that is number`);
+
+console.log(`-----------------------------------------------------------------------------`);
+
+result = 1==true;
+console.log(result);
+console.log(`resulat is true , true is get converted to number`);
+
+console.log(`-----------------------------------------------------------------------------`);
+
+result = 1=='1';
+console.log(result);
+console.log(`result is true , 1 is string get converted to number implicity`);
+
