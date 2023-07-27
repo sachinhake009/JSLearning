5a0bd31517c90d3e1408fe08552de84f18a1c122
sachinhake009 <sachinhake009@gmail.com>
2023-07-27 18:22:18 +0530

    implicit string conversion to number
    implicit string conversion to number

---
+++ 05_implicitStringConversionNumber.js
@@ -0,0 +1,34 @@
+
+
+console.log(`-------------------------String Conversion to Number----------------------------`);
+
+let result;
+
+console.log(`------------------------------------Step 1----------------------------------`);
+
+result = '4' - '2';
+console.log(result);
+ console.log(`when there is arthmetic operator except string then it will get convert to number type , hear 4 is 
+ string it get implicitly converted to number type`);
+
+console.log(`------------------------------------Step 2----------------------------------`);
+
+result = '4' - 2;
+console.log(result);
+console.log(`when there is arthmetic operator except string then it will get convert to number type, hear 4 is 
+string it get implicitly converted to number type`);
+
+console.log(`------------------------------------Step 3----------------------------------`);
+
+result = '4' * 2;
+console.log(result);
+console.log(`when there is arthmetic operator except string then it will get convert to number type , hear 4 is 
+string it get implicitly converted to number type`);
+
+console.log(`------------------------------------Step 4----------------------------------`);
+
+result = '4' / 2;
+console.log(result);
+console.log(`when there is arthmetic operator except string then it will get convert to number type , hear 4 is 
+string it get implicitly converted to number type`);
+
