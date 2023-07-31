fed88ce0191dedf9e1c909cdd5151f77657ea9fe
sachinhake009 <sachinhake009@gmail.com>
2023-07-31 09:47:41 +0530

    For And While Assignment
    For And While Assignment

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,105 @@
+
+
+console.log(`----------------1-----------------`);
+
+for (let index = 5; index <=15; index++) {
+    console.log(index);   
+}
+
+console.log(`----------------2-----------------`);
+
+for (let index = 50; index >=40; index--) {
+    console.log(index);
+    
+}
+
+console.log(`----------------3-----------------`);
+
+for (let index = 1; index < 30; index=index+2) {
+    console.log(index);
+}
+
+console.log(`----------------4-----------------`);
+
+for (let index = 0; index < 20; index=index+2) {
+    console.log(index);
+}
+
+console.log(`----------------5-----------------`);
+
+for (let index = 5; index <= 50; index=index+5) {
+    console.log(index);
+}
+
+console.log(`----------------6-----------------`);
+
+for (let index = 10; index <= 100; index=index+10) {
+    console.log(index);   
+}
+
+console.log(`----------------7-----------------`);
+
+for (let index = 100; index >= 10; index=index-10) {
+    console.log(index);  
+}
+
+console.log(`=============Assignment 2============ `);
+
+console.log(`----------------1-----------------`);
+
+var a = 5;
+while (a<=15) {
+    console.log(a);
+    a=a+1;
+}
+
+console.log(`----------------2-----------------`);
+
+var a = 50;
+while (a>=40) {
+    console.log(a);
+    a=a-1;
+}
+
+console.log(`----------------3-----------------`);
+
+var a = 1;
+while (a<=30) {
+    console.log(a);
+    a=a+2;
+}
+
+console.log(`----------------4-----------------`);
+
+var a = 0;
+while (a<= 20) {
+    console.log(a);
+    a=a+2;
+}
+
+console.log(`----------------5-----------------`);
+
+var a = 5;
+while (a<=50) {
+    console.log(a);
+    a=a+5;
+}
+
+console.log(`----------------6-----------------`);
+
+var a = 10;
+while (a<=100) {
+    console.log(a);
+    a=a+10;
+}
+
+console.log(`----------------7-----------------`);
+
+var a = 100;
+while (a>=10) {
+    console.log(a);
+    a=a-10;
+}
+
+console.log(`===============End===============`);
+
