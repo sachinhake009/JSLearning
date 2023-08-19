a5e1b4f73c7977420ab194f7c79664f36f192232
sachinhake009 <sachinhake009@gmail.com>
2023-08-19 10:29:07 +0530

    JSON
    JSON

--- 14_jsonAssig.js
+++ 14_jsonAssig.js
@@ -1,6 +1,6 @@
 
 
-let jsonString = `
+let jsonDetails = `
 {
 "name": "Aleix Melon",
 "id" : "E00245",
@@ -18,7 +18,7 @@ let jsonString = `
 `
 // console.log(jsonString);
 
-const employeeObj = JSON.parse(jsonString);
+const employeeObj = JSON.parse(jsonDetails);
 
 // Log role as 'DEV'
 console.log("Role:", employeeObj.role[0].toUpperCase());
@@ -32,4 +32,26 @@ console.log("Last Name:", lastName);
 const dojParts = employeeObj.doj.split("-");
 const joiningYear = dojParts[2];
 console.log("Joining Year", joiningYear);
+
+console.log(`------------------------------------------------`);
+
+const employeeData = {
+      "name": "Alex Melon",
+      "id": "E00245",
+      "role": ["Dev", "DBA"],
+      "age": 23,
+      "doj": "11-12-2019",
+      "married": false,
+      "address": {
+        "street": "32, Laham St.",
+        "city": "Innsbruck",
+        "country": "Austria"
+      },
+      "referred-by": "E0012"
+    };
+    
+    console.log("Role:", employeeData.role[0].toUpperCase());
+    console.log("Last Name:", employeeData.name.split(" ")[1]);
+    console.log("Joining Year:", employeeData.doj.split("-")[2]);
+    
  
